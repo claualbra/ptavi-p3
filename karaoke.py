5b665ca1202e319fbbcff80b3e3ccc90f1aa9f41
Claudia Alvarez Bravo <claualbr@alpha22.aulas.gsyc.urjc.es>
2018-10-10 13:52:52 +0200

    karaoke con pep8

--- karaoke.py
+++ karaoke.py
@@ -20,9 +20,9 @@ if __name__ == "__main__":
         sys.exit("Usage:python3 karaoke.py file.smil.")
     lista = cHandler.get_tags()
     for diccionario in lista:
-        print(diccionario['etiqueta'],end="\t")
+        print(diccionario['etiqueta'], end="\t")
         diccionario['etiqueta'] = 'etiqueta'
         for atributo, valor in diccionario.items():
-           if atributo != diccionario['etiqueta'] and valor != "":
-              print(atributo+'='+'"'+ valor+'"',end ="\t")
+            if atributo != diccionario['etiqueta'] and valor != "":
+                print(atributo + '=' + '"' + valor + '"', end="\t")
         print()
