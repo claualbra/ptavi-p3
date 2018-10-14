bbc2aed542d8221e5a53283349fdc42212d13d7f
Usuario generico alumno <claualbra@gmail.com>
2018-10-14 22:31:31 +0200

    añadiendo json

--- karaoke.py
+++ karaoke.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
+import json
 import sys
 import smallsmilhandler
 from xml.sax import make_parser
@@ -16,10 +17,12 @@ if __name__ == "__main__":
     except IndexError:
         sys.exit("Usage:python3 karaoke.py file.smil.")
     lista = cHandler.get_tags()
+    with open('karaoke.json', 'w') as f:
+        json.dump(lista, f)
     for diccionario in lista:
         print(diccionario['etiqueta'], end="\t")
         diccionario['etiqueta'] = 'etiqueta'
         for atributo, valor in diccionario.items():
             if atributo != diccionario['etiqueta'] and valor != "":
-                print(atributo + '=' + '"' + valor + '"', end="\t")
+                print('{0}="{1}"'.format(atributo,valor), end="\t")
         print(end="\n")
