32036f4efaf6e19ce5a3ba92faac4f1b96c1fb27
claualbra <32228581+claualbra@users.noreply.github.com>
2018-10-16 15:21:27 +0200

    Add files via upload

--- karaoke.py
+++ karaoke.py
@@ -8,25 +8,52 @@ import smallsmilhandler
 from xml.sax import make_parser
 
 
+class KaraokeLocal:
+    def __init__(self, file):
+        parser = make_parser()
+        cHandler = smallsmilhandler.SmallSmilHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(file))
+        self.lista = cHandler.get_tags()
+
+    def __str__(self):
+        line_etiqueta = ''
+        for diccionario in self.lista:
+            nom_etiqueta = diccionario['etiqueta']
+            line_etiqueta += diccionario['etiqueta']
+            diccionario['etiqueta'] = 'etiqueta'
+            for atributo, valor in diccionario.items():
+                if atributo != diccionario['etiqueta'] and valor != "":
+                    line_etiqueta += '\t'+'{0}="{1}"'.format(atributo, valor)
+            line_etiqueta += '\n'
+            diccionario['etiqueta'] = nom_etiqueta
+        return line_etiqueta
+
+    def to_json(self, file):
+        file_json = ''
+        if file_json == '':
+            file_json = file.replace('.smil', '.json')
+        with open(file_json, 'w') as f:
+            json.dump(self.lista, f, indent=4)
+
+    def do_local(self):
+        for diccionario in self.lista:
+            for atributo, valor in diccionario.items():
+                if atributo == 'src':
+                    if valor.startswith('http://'):
+                        file_local = valor[valor.rfind('/'):]
+                        urllib.request.urlretrieve(valor, file_local[1:])
+                        diccionario['src'] = file_local[1:]
+
+
 if __name__ == "__main__":
-    parser = make_parser()
-    cHandler = smallsmilhandler.SmallSmilHandler()
-    parser.setContentHandler(cHandler)
     try:
-        parser.parse(open(sys.argv[1]))
+        file = sys.argv[1]
     except IndexError:
         sys.exit("Usage:python3 karaoke.py file.smil.")
-    lista = cHandler.get_tags()
-    with open('karaoke.json', 'w') as file:
-        json.dump(lista, file, indent=4)
-    line_et = ''
-    for diccionario in lista:
-        line_et = diccionario['etiqueta']
-        diccionario['etiqueta'] = 'etiqueta'
-        for atributo, valor in diccionario.items():
-            if valor.startswith('http://'):
-                file_local = valor[valor.rfind('/'):]
-                urllib.request.urlretrieve(valor, file_local[1:])
-            if atributo != diccionario['etiqueta'] and valor != "":
-                line_et += '\t'+'{0}="{1}"'.format(atributo, valor)
-        print(line_et)
+    karaoke = KaraokeLocal(file)
+    print(karaoke)
+    karaoke.to_json(file)
+    karaoke.do_local()
+    karaoke.to_json('local.smil')
+    print(karaoke)
