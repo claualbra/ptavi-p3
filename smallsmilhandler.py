efd5cb7945bd62dd58422251519c92548b6ffb9b
Usuario generico alumno <claualbra@gmail.com>
2018-10-16 21:11:23 +0200

    simplificacion + pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -9,38 +9,19 @@ from xml.sax.handler import ContentHandler
 class SmallSmilHandler(ContentHandler):
     def __init__(self):
         self.lista = []
-        self.diccionario = {}
+        self.dicc_ka = {'root-layout': ['width', 'height', 'background-color'],
+                        'region': ['id', 'top', 'bottom', 'left', 'right'],
+                        'img': ['src', 'region', 'begin', 'dur'],
+                        'audio': ['src', 'begin', 'dur'],
+                        'textstream': ['src', 'region']}
 
     def startElement(self, name, attrs):
-        if name == 'root-layout':
-            self.diccionario = {'etiqueta': name,
-                                'width': attrs.get('width', ""),
-                                'height': attrs.get('height', ""),
-                                'background-color':
-                                attrs.get('background-color', "")}
-            self.lista.append(self.diccionario)
-        elif name == 'region':
-            self.diccionario = {'etiqueta': name, 'id': attrs.get('id', ""),
-                                'top': attrs.get('top', ""),
-                                'bottom': attrs.get('bottom', ""),
-                                'left': attrs.get('left', ""),
-                                'right': attrs.get('right', "")}
-            self.lista.append(self.diccionario)
-        elif name == 'img':
-            self.diccionario = {'etiqueta': name, 'src': attrs.get('src', ""),
-                                'region': attrs.get('region', ""),
-                                'begin': attrs.get('begin', ""),
-                                'dur': attrs.get('dur', "")}
-            self.lista.append(self.diccionario)
-        elif name == 'audio':
-            self.diccionario = {'etiqueta': name, 'src': attrs.get('src', ""),
-                                'begin': attrs.get('begin', ""),
-                                'dur': attrs.get('dur', "")}
-            self.lista.append(self.diccionario)
-        elif name == 'textstream':
-            self.diccionario = {'etiqueta': name, 'src': attrs.get('src', ""),
-                                'region': attrs.get('region', "")}
-            self.lista.append(self.diccionario)
+        diccionario = {}
+        if name in self.dicc_ka:
+            diccionario['etiqueta'] = name
+            for atributo in self.dicc_ka[name]:
+                diccionario[atributo] = attrs.get(atributo, '')
+            self.lista.append(diccionario)
 
     def get_tags(self):
         return self.lista
